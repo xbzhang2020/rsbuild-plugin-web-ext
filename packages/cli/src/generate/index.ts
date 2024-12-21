5a092be4e3faf64bcd986c992c29899b6655631e
zhang <xbzhang2020@icloud.com>
2024-12-21 20:12:17 +0800

    feat(cli): add generate command

--- packages/cli/src/generate/index.ts
+++ packages/cli/src/generate/index.ts
@@ -1,5 +1,5 @@
 import { existsSync } from 'node:fs';
-import { resolve, dirname } from 'node:path';
+import { dirname, resolve } from 'node:path';
 import sharp from 'sharp';
 import { getSrcDir } from '../util.js';
 
