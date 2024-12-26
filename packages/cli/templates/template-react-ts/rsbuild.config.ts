02848bcf0c0e9504f55ba25d6be32b7d8a58c23b
zhang <xbzhang2020@icloud.com>
2024-12-26 08:51:10 +0800

    fix(rsbuild): babel error

--- packages/cli/templates/template-react-ts/rsbuild.config.ts
+++ packages/cli/templates/template-react-ts/rsbuild.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginWebExt } from '../../../rsbuild/dist/index.js';
+import { pluginWebExt } from 'rsbuild-plugin-web-ext';
 
 export default defineConfig({
   plugins: [pluginReact(), pluginWebExt()],
