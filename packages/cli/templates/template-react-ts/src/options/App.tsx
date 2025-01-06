d57bca2f8cfd106aeb51c6727e7f6fc84fdd8173
zhang <xbzhang2020@icloud.com>
2025-01-06 23:00:24 +0800

    feat(cli): support react template

---
+++ packages/cli/templates/template-react-ts/src/options/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="content">
+      <h1>WebExtend + React</h1>
+      <p>This is a options page.</p>
+    </div>
+  );
+}
+
+export default App;
