9a9fbeb2f4b04e41d504d5a26b0d91e840fe8eaf
zhang <xbzhang2020@icloud.com>
2025-01-08 22:01:42 +0800

    feat(cli): support react template

--- packages/cli/templates/template-react-ts/src/content/index.tsx
+++ packages/cli/templates/template-react-ts/src/content/index.tsx
@@ -1,23 +1,21 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import type { ContentScriptConfig } from 'rsbuild-plugin-web-ext';
-import App from './App';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import type { ContentScriptConfig } from "rsbuild-plugin-web-ext";
+import App from "./App";
 
-let rootEl = document.getElementById('web-extend-content');
-
+let rootEl = document.getElementById("web-extend-content");
 if (!rootEl) {
-  rootEl = document.createElement('div');
-  rootEl.id = 'web-extend-content';
+  rootEl = document.createElement("div");
+  rootEl.id = "web-extend-content";
   document.body.appendChild(rootEl);
+  const root = createRoot(rootEl);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 }
 
-const root = createRoot(rootEl);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
-
 export const config: ContentScriptConfig = {
-  matches: ['https://developer.mozilla.org/*'],
+  matches: ["https://developer.mozilla.org/*"],
 };
